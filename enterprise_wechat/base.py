4a45957522674a10df5a9244b37a8d7047280caa
lilacsheep <lilac123>
2018-05-27 21:09:53 +0800

    init

---
+++ enterprise_wechat/base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Client:
+
+    def __init__(self, corpid, corpsecret, agentid):
+        self.corpid, self.corpsecret, self.agentid = corpid, corpsecret, agentid
